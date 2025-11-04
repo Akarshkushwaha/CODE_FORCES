13839b27e81eecb7786d7fd3eb2c78fbe98392a1
Akarsh Kushwaha <akarshkushwaha593@gmail.com>
2025-11-04 23:44:22 +0530

    sss

---
+++ 800_rating/Target Practice.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        vector<string> grid(10);
+
+        for (int i = 0; i < 10; i++)
+        {
+            cin >> grid[i];
+        }
+
+        int total_points = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (grid[i][j] == 'X')
+                {
+
+                    int ring = min(min(i, j), min(9 - i, 9 - j)) + 1;
+
+                    total_points += ring;
+                }
+            }
+        }
+
+        cout << total_points << endl;
+    }
+    return 0;
+}
