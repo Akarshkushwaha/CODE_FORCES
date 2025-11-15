1a080b0922a07016f5a8849032c31a58e35cccfa
Akarsh Kushwaha <akarshkushwaha593@gmail.com>
2025-11-16 01:14:16 +0530

    unit_array

---
+++ 800_rating/unitarray.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+
+        long long a[n];
+        for (long long i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        long long positive_count = 0;
+        long long negative_count = 0;
+
+        for (long long i = 0; i < n; i++)
+        {
+            if (a[i] == 1)
+            {
+                positive_count++;
+            }
+            else
+            {
+                negative_count++;
+            }
+        }
+        long long operation = 0;
+        while (positive_count < negative_count || negative_count % 2 == 1)
+        {
+            operation++;
+            positive_count++;
+            negative_count--;
+        }
+        cout << operation << endl;
+    }
+    return 0;
+}
